04d845228cb7a9f37c82d2890901d6d292066ecd
Sergio Loya <sergio@gammapartners.com>
2016-11-09 11:46:19 -0700

    Toggle Ready

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 'use strict'
 
-var app = angular.module('smart', []);
+var app = angular.module('smart', ['ngMaterial']);
--- js/controllers/actionController.js
+++ js/controllers/actionController.js
@@ -5,24 +5,36 @@ app.controller('actionController',
 	function actionController ($scope) {
 
 		$scope.checkboxModel = {
-			value1 : true
+			value1 : false
 		};
 
-		$scope.turnOnOff = function () {
-    	console.log(checkboxModel.value1);
-			if(checkboxModel.value1) {
+		$scope.turnOnOff = function (dato) {
+    	console.log(dato);
+			if(dato) {
 				socket.emit('turnOn');
 			}else {
 				socket.emit('turnOff')
 			}
 		}
 
-
 		$scope.turnOff = function () {
 			socket.emit('turnOff');
 		}
 
-			socket.on('turnOn', function(){
-				console.log('recived socket on')
+		socket.on('turnOn', function(){
+			console.log('recived socket on')
+		});
+
+		socket.on("lectura", function(lectura) {
+			$scope.$apply(function(){
+				if (lectura == "1"){
+					$scope.checkboxModel.value1 = true;
+					$scope.data = "Led is ON";
+				}
+				else if (lectura == "0") {
+					$scope.checkboxModel.value1 = false;
+					$scope.data = "Led is OFF";
+				}
 			});
+		});
 	});
